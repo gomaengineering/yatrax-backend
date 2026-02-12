10100053b486b28f3e2e50c7b414987cd5acf07c
Ujwol Aryal <ujwolaryal@gmail.com>
2026-02-12 14:09:55 +0545

    Forgot password

--- src/routes/web/userAuthRoutes.js
+++ src/routes/web/userAuthRoutes.js
@@ -1,12 +1,21 @@
 import express from "express";
-import { googleLogin, loginUser, registerUser, getGoogleClientId } from "../../controllers/web/userAuthController.js";
+import {
+  getGoogleClientId,
+  googleLogin,
+  loginUser,
+  registerUser,
+  forgotPassword,
+  resetPassword,
+} from "../../controllers/web/userAuthController.js";
 
 const router = express.Router();
 
-router.get("/google-client-id", getGoogleClientId); // Public endpoint to get Google Client ID
+router.get("/google-client-id", getGoogleClientId);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/google-login", googleLogin); // Google login route
+router.post("/google-login", googleLogin);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 
 export default router;
 
